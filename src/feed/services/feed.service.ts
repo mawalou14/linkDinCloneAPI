30a79dcece11f2dd36c8226f0ef1036884635445
mawalou14 <awaloum213@gmail.com>
2024-01-03 16:10:56 +0100

    create post method

--- src/feed/services/feed.service.ts
+++ src/feed/services/feed.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { FeedPostEntity } from '../modules/posts.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { FeedPost } from '../modules/posts.interface';
 
 @Injectable()
 export class FeedService {
@@ -9,4 +10,8 @@ export class FeedService {
         @InjectRepository(FeedPostEntity)
         private readonly feedPostRepository: Repository<FeedPostEntity>
     ) {}
+
+    createPost(feedPost: FeedPost) {
+        return this.feedPostRepository.save(feedPost);
+    }
 }
