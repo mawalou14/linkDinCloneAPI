5c5d9520db306c9ccc8b3342eb36c98439221f34
mawalou14 <awaloum213@gmail.com>
2024-01-03 15:52:54 +0100

    create feed module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeedModule } from './feed/feed.module';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.POSTGRES_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
-    })
+    }),
+    FeedModule
   ],
   controllers: [AppController],
   providers: [AppService],
