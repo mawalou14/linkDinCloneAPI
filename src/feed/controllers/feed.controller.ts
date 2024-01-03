c355d8d848b5ac6905f4db80856cd57c8f6ab84e
mawalou14 <awaloum213@gmail.com>
2024-01-03 16:48:38 +0100

    update post function

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -1,13 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { FeedService } from '../services/feed.service';
 import { FeedPost } from '../modules/posts.interface';
 import { Observable } from 'rxjs';
+import { UpdateResult } from 'typeorm';
 
 @Controller('feed')
 export class FeedController {
     constructor(
         private feedService: FeedService
-    ) {  }
+    ) { }
     @Post()
     createPost(@Body() post: FeedPost): Observable<FeedPost> {
         return this.feedService.createPost(post);
@@ -16,5 +17,10 @@ export class FeedController {
     @Get()
     findAllPosts(): Observable<FeedPost[]> {
         return this.feedService.findAllPost();
+    }
+
+    @Put(':id')
+    updatePost(@Param('id') id: number, @Body() feedPost: FeedPost): Observable<UpdateResult> {
+        return this.feedService.updatePost(id, feedPost)
     }
 }
