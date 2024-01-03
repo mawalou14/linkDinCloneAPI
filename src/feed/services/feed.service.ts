feb9885107ee95c7d42ba13428c382b5a62d5f5b
mawalou14 <awaloum213@gmail.com>
2024-01-03 16:09:50 +0100

    injecting repository

--- src/feed/services/feed.service.ts
+++ src/feed/services/feed.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { FeedPostEntity } from '../modules/posts.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class FeedService {}
+export class FeedService {
+    constructor(
+        @InjectRepository(FeedPostEntity)
+        private readonly feedPostRepository: Repository<FeedPostEntity>
+    ) {}
+}
