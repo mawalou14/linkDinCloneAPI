e4ae300bd2baaa42334988572162736c874f33a8
mawalou14 <awaloum213@gmail.com>
2024-01-03 16:12:10 +0100

    inject service

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Post } from '@nestjs/common';
+import { FeedService } from '../services/feed.service';
 
 @Controller('feed')
 export class FeedController {
+    constructor(
+        private feedService: FeedService
+    ) {}
     @Post()
     create() {
         
