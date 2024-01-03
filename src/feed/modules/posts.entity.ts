0ef6ab72d82c8b023b76c2a390c6f6826f286893
mawalou14 <awaloum213@gmail.com>
2024-01-03 16:04:31 +0100

    created feed post entity

--- src/feed/modules/posts.entity.ts
+++ src/feed/modules/posts.entity.ts
@@ -1,6 +1,13 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('feed_post')
 export class FeedPostEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
 
+    @Column({ default: '' }) 
+    body: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' }) 
+    created_at: Date;    
 }
