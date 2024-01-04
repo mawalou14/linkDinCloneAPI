86ae0ed5358131bbaf4662289c27ce34b58a6363
mawalou14 <awaloum213@gmail.com>
2024-01-04 23:00:15 +0100

    still with user entity

--- src/auth/modules/user.entity.ts
+++ src/auth/modules/user.entity.ts
@@ -1,7 +1,20 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity('user')
 export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
     
+    @Column()
+    lastName: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    role: Role;
 }
