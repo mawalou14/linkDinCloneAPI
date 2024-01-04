891f21d9ebe1ed01fc1738f3ac9350bc0dc599d5
mawalou14 <awaloum213@gmail.com>
2024-01-05 00:12:26 +0100

    add jwtfrom request in constructo

--- src/auth/guards/jwt.strategy.ts
+++ src/auth/guards/jwt.strategy.ts
@@ -1,8 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET,
+        });
+    }
+
 }
