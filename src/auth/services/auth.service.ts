ae0535a1c34ac31209287e65d63c7daa1d7b174a
mawalou14 <awaloum213@gmail.com>
2024-01-04 23:19:22 +0100

    crete hashPass return

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 import * as bcrypt from 'bcrypt';
+import { Observable, from } from 'rxjs';
 
 @Injectable()
 export class AuthService {
     contructor() {}
 
-    hashPassword() {
-        
+    hashPassword(password: string): Observable<string> {
+        return from(bcrypt.hash(password, 12));
     }
 
 }
