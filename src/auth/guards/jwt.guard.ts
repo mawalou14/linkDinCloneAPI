904b77987c2940ae7732366cbfc78096425a3f65
mawalou14 <awaloum213@gmail.com>
2024-01-04 23:55:43 +0100

    create auth guard method

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,11 +1,8 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
+export class JwtGuard extends AuthGuard('jwt') {
+  
 }
