57c2f32c09cdf16994b8f6bfada7b0ce8218a08a
mawalou14 <awaloum213@gmail.com>
2024-01-05 00:02:51 +0100

    add jwt in authModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './modules/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '36000s' }
+      })
+    }),
     TypeOrmModule.forFeature([UserEntity])
   ],
   providers: [AuthService],
