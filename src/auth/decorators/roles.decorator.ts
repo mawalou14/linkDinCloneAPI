c65009d84646a0e22a6b123f98f89bf9a3a91c7d
mawalou14 <awaloum213@gmail.com>
2024-01-05 23:28:50 +0100

    exporting role key constant

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,3 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const Roles = (...args: string[]) => SetMetadata('roles', args);
+export const ROLES_KEY = 'roles';
+export const Roles = (...args: string[]) => SetMetadata(ROLES_KEY, args);
