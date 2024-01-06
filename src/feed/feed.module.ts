a0acea176cb23460d0129f32505b60d92af22c49
mawalou14 <awaloum213@gmail.com>
2024-01-06 17:26:26 +0100

    import authModule

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -3,9 +3,11 @@ import { FeedService } from './services/feed.service';
 import { FeedController } from './controllers/feed.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeedPostEntity } from './modules/posts.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
     imports: [
+      AuthModule,
         TypeOrmModule.forFeature([FeedPostEntity])
     ],
   providers: [FeedService],
