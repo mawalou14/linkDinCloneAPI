74d8255b15fb17938485999df274a8016c64a68c
mawalou14 <awaloum213@gmail.com>
2024-01-06 23:06:26 +0100

    inject services

--- src/feed/guards/is-creator.guard.ts
+++ src/feed/guards/is-creator.guard.ts
@@ -1,8 +1,15 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/auth/services/auth.service';
+import { FeedService } from '../services/feed.service';
 
 @Injectable()
 export class IsCreatorGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private feedService: FeedService
+    ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
